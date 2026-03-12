eae1fed75e8d5ea6ff6ad3388e28598009eaec43
yashpatel0077 <yashpatel30648@gmail.com>
2026-03-12 19:12:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 <?php
 
 $host = "localhost";          // Hostinger MySQL host (usually 'localhost')
-$db   = "u393163730_messaging_web";  // CHANGE: your database name
-$user = "u393163730_web";  // CHANGE: your database username
-$pass = "Yashpatel30648";  // CHANGE: your database password
+$db   = " ";  // CHANGE: your database name
+$user = " ";  // CHANGE: your database username
+$pass = " ";  // CHANGE: your database password
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 
@@ -20,3 +20,4 @@ $conn = $mysqli;
 // Optional: set charset to utf8mb4 for emojis, multilingual text
 $mysqli->set_charset("utf8mb4");
 ?>
+
